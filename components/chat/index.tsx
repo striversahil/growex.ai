871879cffa985df3da39d69400a7c993066ce2d9
Sahil Kumar <striversahil@gmail.com>
2025-08-08 07:32:47 +0530

    refactor: enhance ChatWindow to display chat messages with user and AI responses

--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -22,8 +22,9 @@ const ChatInterface = (props: Props) => {
       <PopoverContent className=" bg-white/10 border border-white/15 w-100 h-[50vh] p-4 rounded-3xl">
         <div className="absolute flex flex-col inset-2 bg-background rounded-3xl">
           {/* A Window of Messages */}
-          <div className="flex-1 overflow-y-auto"></div>
-          <ChatWindow />
+          <div className="flex-1 overflow-y-auto p-2">
+            <ChatWindow />
+          </div>
           <form
             className="space-y-4 px-2 py-2"
             onSubmit={(e) => {
--- components/chat/window.tsx
+++ components/chat/window.tsx
@@ -2,8 +2,35 @@ import React from "react";
 
 type Props = {};
 
+const chats = [
+  { user: "User", message: "Hello AI" },
+  { user: "AI", message: "I am here to assist you with your queries." },
+  { user: "User", message: "What is the weather like today?" },
+  { user: "AI", message: "The weather is sunny with a high of 25°C." },
+];
+
 const ChatWindow = (props: Props) => {
-  return <div>ChatWindow</div>;
+  return (
+    <div className="flex-1 overflow-y-auto rounded-lg shadow-md">
+      {chats.map((chat, index) => (
+        <div key={index}>
+          {chat.user === "User" ? (
+            <div className="flex justify-end">
+              <div className=" bg-blue-600 p-2 rounded-full mb-2">
+                <strong>{chat.user}:</strong> {chat.message}
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-start">
+              <div className="bg-green-600 p-2 rounded-full mb-2">
+                <strong>{chat.user}:</strong> {chat.message}
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatWindow;
