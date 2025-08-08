8108c40176ae3edad4785b7567780060f92b52e3
Sahil Kumar <striversahil@gmail.com>
2025-08-08 07:23:28 +0530

    add ChatWindow component and integrate it into ChatInterface

--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -7,6 +7,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import ChatWindow from "./window";
 
 type Props = {};
 
@@ -18,15 +19,24 @@ const ChatInterface = (props: Props) => {
           AI Agent
         </Button>
       </PopoverTrigger>
-      <PopoverContent className=" bg-white/10 border border-white/15 w-100 h-200 p-4 rounded-3xl">
-        <div className="absolute inset-2 bg-background rounded-3xl">
-          <div className="space-y-4">
-            <Label htmlFor="query">Ask your question</Label>
-            <Input id="query" placeholder="Type your question here..." />
-            <Button type="submit" className="w-full bg-blue-500 text-white">
-              Send
-            </Button>
-          </div>
+      <PopoverContent className=" bg-white/10 border border-white/15 w-100 h-[50vh] p-4 rounded-3xl">
+        <div className="absolute flex flex-col inset-2 bg-background rounded-3xl">
+          {/* A Window of Messages */}
+          <div className="flex-1 overflow-y-auto"></div>
+          <ChatWindow />
+          <form
+            className="space-y-4 px-2 py-2"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(e.target.query.value);
+            }}
+          >
+            <Input
+              id="query"
+              placeholder="Type your question here..."
+              className=" w-full rounded-3xl "
+            />
+          </form>
         </div>
       </PopoverContent>
     </Popover>
---
+++ components/chat/window.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ChatWindow = (props: Props) => {
+  return <div>ChatWindow</div>;
+};
+
+export default ChatWindow;
